cbe1d27b67015e1d1d580e146aa786e05bd13bd1
Justin Menga <justin.menga@gmail.com>
2020-04-27 13:59:19 +1200

    Use in-memory database for API

---
+++ src/lib/todos.js
@@ -0,0 +1,27 @@
+module.exports = () => {
+  const todos = {
+    "todos": [
+      {
+        "id": 1,
+        "title": "Buy Milk",
+        "completed": false
+      },
+      {
+        "id": 2,
+        "title": "Buy Eggs",
+        "completed": true
+      },
+      {
+        "id": 3,
+        "title": "Buy Bread",
+        "completed": false
+      },
+      {
+        "id": 4,
+        "title": "Make French Toast",
+        "completed": false
+      }
+    ]
+  }
+  return todos
+}
